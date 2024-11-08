fa7eac65a5dbe07cb75c31148191a5c4dcb7c500
lucasfilp <lucasfmaq1@gmail.com>
2024-11-08 10:23:01 -0300

    corrigido

--- erros.py
+++ erros.py
@@ -1,29 +1,30 @@
 
 # Função para calcular o quadrado dos números em uma lista
-def calcula_quadrado(lista_numeros)
+def calcula_quadrado(lista_numeros):
     quadrados = []
     for numero in lista_numeros:
-    quadrado = numero ** 2 
+        quadrado = numero ** 2 
         quadrados.append(quadrado)  
 
-    return quadrado  # ERRO: deve retornar 'quadrados' em vez de 'quadrado'
+    return quadrados  # ERRO: deve retornar 'quadrados' em vez de 'quadrado'
 
 # Função para verificar se um número é positivo, negativo ou zero
 def verifica_positivo_negativo_zero(num):
-    if num > 0
+    if num > 0:
         return "Positivo"  
-    elif num < 0
+    elif num < 0:
         return "Negativo" 
-    else
-    return "Zero"  
+    else:
+        return "Zero"  
+
 def calcula_media(lista_numeros):
     soma = 0
-    for numero in lista_numeros
-        soma += numero  
+    for numero in lista_numeros:
+        soma += numero
     if len(lista_numeros) == 0:
-        return "A lista está vazia, não é possível calcular a média" 
+        return "A lista está vazia, não é possível calcular a média"
     media = soma / len(lista_numeros)
-     return media  
+    return media  
 
 # Testando o código
 numeros = [2, -3, 0, 5, 7]
